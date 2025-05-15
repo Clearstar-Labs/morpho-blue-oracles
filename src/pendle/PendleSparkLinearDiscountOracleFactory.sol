42e88cc81dcc82a62d2b666d72b5e72df3db15d1
Ulver <ulver@bytemasons.com>
2025-05-15 16:25:42 +0200

    Add linear discount oracle

---
+++ src/pendle/PendleSparkLinearDiscountOracleFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.21;
+
+import {PendleSparkLinearDiscountOracle} from "./PendleSparkLinearDiscountOracle.sol";
+import {IPendleSparkLinearDiscountOracleFactory} from "./interfaces/IPendleSparkLinearDiscountOracleFactory.sol";
+
+/// @title PendleSparkLinearDiscountOracleFactory
+/// @notice This contract allows creation of PendleSparkLinearDiscountOracle oracles and indexes them for verification
+contract PendleSparkLinearDiscountOracleFactory is
+    IPendleSparkLinearDiscountOracleFactory
+{
+    /// @inheritdoc IPendleSparkLinearDiscountOracleFactory
+    mapping(address => bool) public isPendleSparkLinearDiscountOracle;
+
+    /// @inheritdoc IPendleSparkLinearDiscountOracleFactory
+    function createPendleSparkLinearDiscountOracle(
+        address _pt,
+        uint256 _baseDiscountPerYear,
+        bytes32 salt
+    ) external returns (PendleSparkLinearDiscountOracle oracle) {
+        oracle = new PendleSparkLinearDiscountOracle{salt: salt}(
+            _pt,
+            _baseDiscountPerYear
+        );
+
+        isPendleSparkLinearDiscountOracle[address(oracle)] = true;
+        emit CreatePendleSparkLinearDiscountOracle(msg.sender, address(oracle));
+    }
+}
